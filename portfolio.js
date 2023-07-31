5f3f5673002129852981b64d6b4c7df2320a4aff
Success224 <139138764+Success224@users.noreply.github.com>
2023-07-31 21:03:19 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+/* ======================================= typing animation ====================== */
+var typed = new Typed(".typing",{
+    strings: ['','Web Developer','Graphic Designer','Product Designer'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
